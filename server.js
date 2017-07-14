b32813151f19928d222b79f57a73419d6122825c
Encho Enevski <encho.enevski@gmail.com>
2017-07-14 10:53:59 +0300

    add mongo to our project

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const app = require("./app/app");
 
 const config = require('./config');
 
+const db = require('./db').init(config.connectionString);
+
 app.listen(config.port, function() {
     console.log("Server is running at http://localhost:" + config.port);
 });
